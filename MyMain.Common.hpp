45030552dd1312679a1720362f16d4ee3ef643ee
James Schumacher <james_schumacher_california@outlook.com>
2017-07-05 18:24:42 -0700

    Update and rename MyMain.Common.h to MyMain.Common.hpp

---
+++ MyMain.Common.hpp
@@ -0,0 +1,11 @@
+#ifndef J__MYMAIN_COMMON_HPP
+#define J__MYMAIN_COMMON_HPP
+
+namespace James_Common
+{
+    typedef char ApiChar, ApiSignedInt8;
+    typedef unsigned char ApiByte, ApiUnsigned8;
+    
+}
+
+#endif
