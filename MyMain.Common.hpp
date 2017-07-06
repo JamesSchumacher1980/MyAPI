807b3e08f5ca509ad6c042d60f5b313aeec603e2
James Schumacher <james_schumacher_california@outlook.com>
2017-07-05 18:31:25 -0700

    Update MyMain.Common.hpp

--- MyMain.Common.hpp
+++ MyMain.Common.hpp
@@ -5,6 +5,13 @@ namespace James_Common
 {
     typedef char ApiChar, ApiSignedInt8;
     typedef unsigned char ApiByte, ApiUnsigned8;
+    typedef wchar_t ApiUniChar;
+    typedef short ApiSigned16, ApiShort;
+    typedef unsigned short ApiUnsigned16,
+        ApiUShort;
+    typedef long ApiSigned32, ApiLong32;
+    typedef unsigned long ApiUnsigned32,
+        ApiLong32;
     
 }
 
